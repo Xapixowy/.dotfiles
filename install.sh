e9d51f6318478cade43ae3698616cb69024dcc93
Jakub Chodzinski <xapixowy@gmail.com>
2025-05-22 17:18:21 +0200

    feat: add git configuration with some tweaks ✨

--- install.sh
+++ install.sh
@@ -1,22 +1,133 @@
 #!/bin/bash
 
-install_tpm_if_missing() {
-  echo "Checking for and cloning TPM for tmux..."
-  if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
-    git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
-    echo "TPM has been cloned."
+START_SYM="===>"
+SUCCESS_SYM="[OK]"
+ERROR_SYM="[ERROR]"
+INFO_SYM="[INFO]"
+FIRST_LEVEL_SYM=" •"
+
+link_dotfiles() {
+  echo "$START_SYM Linking dotfiles with Stow..."
+  stow .
+  local stow_exit_code=$?
+
+  if [ $stow_exit_code -eq 0 ]; then
+    echo "$FIRST_LEVEL_SYM $SUCCESS_SYM Dotfiles linked."
+  elif [ $stow_exit_code -eq 1 ]; then
+    echo "$FIRST_LEVEL_SYM $ERROR_SYM Stow: Conflict. Files already exist."
+  elif [ $stow_exit_code -eq 2 ]; then
+    echo "$FIRST_LEVEL_SYM $ERROR_SYM Stow: Invalid usage or package issue."
   else
-    echo "TPM already exists, skipping cloning."
+    echo "$FIRST_LEVEL_SYM $ERROR_SYM Stow: Unknown error (code: $stow_exit_code)."
   fi
 }
 
-link_dotfiles() {
-  echo "Linking dotfiles using Stow..."
-  stow .
-  echo "Dotfiles have been linked."
+install_homebrew_if_missing() {
+  echo "$START_SYM Checking for Homebrew..."
+  if ! command -v brew &> /dev/null; then
+    echo "$FIRST_LEVEL_SYM $INFO_SYM Homebrew not found, installing..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo "$FIRST_LEVEL_SYM $SUCCESS_SYM Homebrew installed."
+  else
+    echo "$FIRST_LEVEL_SYM $INFO_SYM Homebrew already installed, skipping installation."
+  fi
 }
 
-install_tpm_if_missing
-link_dotfiles
+install_oh_my_zsh_if_missing() {
+  echo "$START_SYM Checking for and cloning Oh My Zsh..."
+  if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    git clone https://github.com/ohmyzsh/ohmyzsh "$HOME/.oh-my-zsh"
+    echo "$FIRST_LEVEL_SYM $SUCCESS_SYM Oh My Zsh has been cloned."
+  else
+    echo "$FIRST_LEVEL_SYM $INFO_SYM Oh My Zsh already exists, skipping cloning."
+  fi
+}
 
-echo "Installation complete!"
+install_starship_if_missing() {
+  echo "$START_SYM Checking for Starship (via Brew)..."
+
+  if command -v starship &>/dev/null; then
+    echo "$FIRST_LEVEL_SYM $INFO_SYM Starship is already installed. Skipping installation."
+  else
+    brew install starship
+    local brew_exit_code=$?
+
+    if [ $brew_exit_code -eq 0 ]; then
+      echo "$FIRST_LEVEL_SYM $SUCCESS_SYM Starship installed successfully."
+    else
+      echo "$FIRST_LEVEL_SYM $ERROR_SYM Failed to install Starship with Brew (exit code: $brew_exit_code)."
+    fi
+  fi
+}
+
+install_zsh_completions() {
+  echo "$START_SYM Checking for and cloning zsh-completions plugin..."
+  if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-completions" ]; then
+    git clone https://github.com/zsh-users/zsh-completions "$HOME/.oh-my-zsh/custom/plugins/zsh-completions"
+    echo "$FIRST_LEVEL_SYM $SUCCESS_SYM zsh-completions plugin has been cloned."
+  else
+    echo "$FIRST_LEVEL_SYM $INFO_SYM zsh-completions plugin already exists, skipping cloning."
+  fi
+}
+
+install_zsh_syntax_highlighting() {
+  echo "$START_SYM Checking for and cloning zsh-syntax-highlighting plugin..."
+  if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
+    echo "$FIRST_LEVEL_SYM $SUCCESS_SYM zsh-syntax-highlighting plugin has been cloned."
+  else
+    echo "$FIRST_LEVEL_SYM $INFO_SYM zsh-syntax-highlighting plugin already exists, skipping cloning."
+  fi
+}
+
+install_zsh_autosuggestions_plugin_if_missing() {
+  echo "$START_SYM Checking for and cloning zsh-autosuggestions plugin..."
+  if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
+    echo "$FIRST_LEVEL_SYM $SUCCESS_SYM zsh-autosuggestions plugin has been cloned."
+  else
+    echo "$FIRST_LEVEL_SYM $INFO_SYM zsh-autosuggestions plugin already exists, skipping cloning."
+  fi
+}
+
+install_tmux_tpm_if_missing() {
+  echo "$START_SYM Checking for and cloning TPM for tmux..."
+  if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+    git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
+    echo "$FIRST_LEVEL_SYM $SUCCESS_SYM TPM has been cloned."
+  else
+    echo "$FIRST_LEVEL_SYM $INFO_SYM TPM already exists, skipping cloning."
+  fi
+}
+
+install_diff_so_fancy() {
+  echo "$START_SYM Checking for diff-so-fancy (via Brew)..."
+
+  if command -v diff-so-fancy &>/dev/null; then
+    echo "$FIRST_LEVEL_SYM $INFO_SYM diff-so-fancy is already installed. Skipping installation."
+  else
+    brew install diff-so-fancy
+    local brew_exit_code=$?
+
+    if [ $brew_exit_code -eq 0 ]; then
+      echo "$FIRST_LEVEL_SYM $SUCCESS_SYM diff-so-fancy installed successfully."
+    else
+      echo "$FIRST_LEVEL_SYM $ERROR_SYM Failed to install diff-so-fancy with Brew (exit code: $brew_exit_code)."
+    fi
+  fi
+}
+
+main() {
+  # link_dotfiles
+  install_homebrew_if_missing
+  install_oh_my_zsh_if_missing
+  install_starship_if_missing
+  install_zsh_completions
+  install_zsh_syntax_highlighting
+  install_zsh_autosuggestions_plugin_if_missing
+  install_tmux_tpm_if_missing
+  install_diff_so_fancy
+}
+
+main
+echo -e "\n🚀 Installation complete!"
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,31 +1,53 @@
-# Starship
+# Load: Oh My Zsh
+export ZSH="$HOME/.oh-my-zsh"
+ZSH_THEME=""
+plugins=(git zsh-autosuggestions zsh-syntax-highlighting zsh-completions)
+source $ZSH/oh-my-zsh.sh
+
+# Load: Starship
 eval "$(starship init zsh)"
 
-# Enable CLI Colors
-export CLICOLOR=1
-
-# Load Node Version Manager
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-# Load Angular CLI autocompletion.
+# Load: Angular CLI
 source <(ng completion script)
 
-# Load Brew
+# Load: Brew
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-# Aliases
+# Load: bun
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+[ -s "/Users/xapixowy/.bun/_bun" ] && source "/Users/xapixowy/.bun/_bun"
+
+# Load: nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+# Aliases: General
 alias ls='ls -G'
 alias ll='ls -lG'
 alias neofetch='fastfetch'
 
-# bun completions
-[ -s "/Users/xapixowy/.bun/_bun" ] && source "/Users/xapixowy/.bun/_bun"
+# Aliases: Git
+alias ga='git add'
+alias gap='ga --patch'
+alias gb='git branch'
+alias gba='gb --all'
+alias gc='git commit'
+alias gca='gc --amend --no-edit'
+alias gce='gc --amend'
+alias gco='git checkout'
+alias gcl='git clone --recursive'
+alias gd='git diff --output-indicator-new=" " --output-indicator-old=" "'
+alias gds='gd --staged'
+alias gi='git init'
+alias gl='git log --graph --all --pretty=format:"%C(magenta)%h %C(white) %an  %ar%C(red)  %D%n%s%n"'
+alias gm='git merge'
+alias gn='git checkout -b'  # new branch
+alias gp='git push'
+alias gr='git reset'
+alias gs='git status --short'
+alias gu='git pull'
 
-# bun
-export BUN_INSTALL="$HOME/.bun"
-export PATH="$BUN_INSTALL/bin:$PATH"
-
-# Show random Pokemon on startup
+# Display random Pokemon
 pokemon-colorscripts -r --no-title
